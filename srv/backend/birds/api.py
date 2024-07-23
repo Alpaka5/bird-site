56096829ea796442f7bee1b88cad9b2dc6098420
PM34NO <maciej.boczarski@ing.com>
2024-07-23 07:27:16 +0200

    Added excel file with birds names

--- srv/backend/birds/api.py
+++ srv/backend/birds/api.py
@@ -1,5 +1,12 @@
-from ninja import Router, ModelSchema, Schema
+from urllib.request import Request
+
+from ninja import Router, ModelSchema, Schema, File
+from ninja.files import UploadedFile
+
+import openpyxl
+
 from birds.models import Bird
+
 
 router = Router()
 
@@ -7,12 +14,13 @@ router = Router()
 class BirdSchema(ModelSchema):
     class Meta:
         model = Bird
-        exclude=["id"]
+        fields="__all__"
 
 class Error(Schema):
     message: str
 
-@router.get("/id/{bird_id}", response={"200": BirdSchema, 404: Error})
+
+@router.get("/id/{bird_id}", response={200: BirdSchema, 404: Error})
 def get_bird_by_id(
     request,
     bird_id: int,
@@ -21,3 +29,21 @@ def get_bird_by_id(
         return 200, Bird.objects.get(id=bird_id)
     except Bird.DoesNotExist:
         return 404, {"message": f"Bird with id: {bird_id} doesn't exist"}
+
+
+class TempResponse(Schema):
+    message: str
+
+
+@router.post("/db/update")
+def update_db_with_birds(
+    request: Request,
+    file: UploadedFile = File(...),
+    response={200: TempResponse, 422: Error},
+):
+    if not file.name.endswith(".xlsx"):
+        return 422, Error(message="Unsupported file format. Please upload a .xlsx file")
+
+    workbook = openpyxl.load_workbook(file)
+
+    return 200, TempResponse(message=f"{workbook.sheetnames}")
--- srv/backend/birds/models.py
+++ srv/backend/birds/models.py
@@ -1,35 +1,65 @@
 from django.db import models
 
 
+class Language(models.Model):
+    code = models.CharField(
+        max_length=3,
+        primary_key=True,
+    )
+
+
 class BirdOrder(models.Model):
-    name = models.CharField(max_length=50, primary_key=True)
+    latin_name = models.CharField(max_length=50, primary_key=True)
+
+
+class BirdOrderTranslation(models.Model):
+    order = models.ForeignKey(BirdOrder, on_delete=models.CASCADE)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
 
 
 class BirdSuborder(models.Model):
-    name = models.CharField(max_length=50, primary_key=True)
+    latin_name = models.CharField(max_length=50, primary_key=True)
     order = models.ForeignKey(BirdOrder, on_delete=models.CASCADE)
 
 
+class BirdSuborderTranslation(models.Model):
+    suborder = models.ForeignKey(BirdSuborder, on_delete=models.CASCADE)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+
 class BirdFamily(models.Model):
-    name = models.CharField(max_length=50, primary_key=True)
+    latin_name = models.CharField(max_length=50, primary_key=True)
     suborder = models.ForeignKey(BirdSuborder, on_delete=models.CASCADE)
 
 
+class BirdFamilyTranslation(models.Model):
+    family = models.ForeignKey(BirdFamily, on_delete=models.CASCADE)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+
+
 class Bird(models.Model):
-    latin_name = models.CharField(max_length=50, unique=True)
-    length_mm = models.IntegerField()
-    weight_g = models.IntegerField()
+    latin_name = models.CharField(max_length=50, primary_key=True)
+    length_min_mm = models.IntegerField()
+    length_max_mm = models.IntegerField()
+    weight_min_g = models.IntegerField()
+    weight_max_g = models.IntegerField()
     family = models.ForeignKey(BirdFamily, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.latin_name
 
 
-class Language(models.Model):
-    code = models.CharField(
-        max_length=3,
-        primary_key=True,
-    )
+class BirdSoundsURL(models.Model):
+    SONG = "song"
+    CALL = "call"
+    MIXED = "mixed"
+    SOUND_TYPE = {SONG: "song", CALL: "call", MIXED: "mixed"}
+    bird = models.ForeignKey(Bird, on_delete=models.CASCADE)
+    url = models.CharField(max_length=300)
+    type = models.CharField(max_length=100, choices=SOUND_TYPE)
 
 
 class BirdTextField(models.Model):
