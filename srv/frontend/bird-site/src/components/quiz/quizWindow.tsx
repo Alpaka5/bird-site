1138082bdd68ae8f83cef2b46bbea0f5d57ca2dc
PM34NO <maciej.boczarski@ing.com>
2024-11-06 13:54:11 +0100

    Added quiz game creator

--- srv/frontend/bird-site/src/components/quiz/quizWindow.tsx
+++ srv/frontend/bird-site/src/components/quiz/quizWindow.tsx
@@ -54,13 +54,13 @@ function QuizAnswer({bird, onClick}: { bird: birdDetailed, collapseState: Object
 
     return (
         <div id={"col_quiz_ans_" + bird.latin_name}
-             className="flex justify-center items-center col-start-1 col-end-1 max-w-64 w-full
-             answer-button-gray p-5 rounded-xl hover:outline-gray-500 hover:outline hover:outline-2"
+             className="flex justify-center items-center col-start-1 col-end-1 min-w-64 w-full
+             answer-button button-gray p-5 rounded-xl"
              onClick={(event) => {
                  onClick(event)
              }}
              data-winning={bird.winning_bird}>
-            <>{capitalizeFirstLetter(bird.latin_name)}</>
+            <span>{capitalizeFirstLetter(bird.latin_name)}</span>
         </div>
     )
 }
@@ -79,9 +79,9 @@ function QuizAnswers({quizQuery}: { quizQuery: UseQueryResult }) {
             for (const single_bird of quizGame.birds) {
                 let answerItem = document.getElementById("col_quiz_ans_" + single_bird.latin_name);
                 if (answerItem.dataset.winning == 'true') {
-                    answerItem.classList.replace("answer-button-gray", "answer-button-green");
+                    answerItem.classList.replace("button-gray", "button-green");
                 } else {
-                    answerItem.classList.replace("answer-button-gray", "answer-button-red");
+                    answerItem.classList.replace("button-gray", "button-red");
                 }
 
 
@@ -91,18 +91,20 @@ function QuizAnswers({quizQuery}: { quizQuery: UseQueryResult }) {
                 setCorrectAnswersCounter(correctAnswersCounter + 1);
             }
             let quizBirdDescription = document.getElementById("quiz-bird-description");
-            quizBirdDescription.hidden = false;
-
-            let quizAnswerGrid = document.getElementById("quiz-answer-grid");
-            quizAnswerGrid.style.gridTemplateColumns = "1fr 3fr"
-            quizAnswerGrid.style.columnGap='6rem';
+            quizBirdDescription.classList.remove("hidden");
+            setTimeout(function () {
+                quizBirdDescription.classList.replace("quiz-description-hidden", "quiz-description");
+            }, 10);
+            // let quizAnswerGrid = document.getElementById("quiz-answer-grid");
+            // quizAnswerGrid.style.gridTemplateColumns = "1fr 3fr"
+            // quizAnswerGrid.style.columnGap = '6rem';
         }
 
         setBirdDescription(<BirdDescriptionWindow selectedBird={bird}/>)
 
         let newGameButton: HTMLElement = document.getElementById("new_quiz_button");
-        newGameButton.hidden = false;
-
+        newGameButton.style.visibility = "visible";
+        newGameButton.style.opacity = 1;
         setQuizAnswered(true);
     }
 
@@ -116,28 +118,32 @@ function QuizAnswers({quizQuery}: { quizQuery: UseQueryResult }) {
     }
 
     function startNewQuiz(event) {
+
         let quizBirdSound: HTMLElement = document.getElementById("quiz_bird_sound")
+
         quizBirdSound.pause()
         quizBirdSound.currentTime = 0
 
         let quizBirdDescription = document.getElementById("quiz-bird-description");
-        quizBirdDescription.hidden = true;
 
-        let quizAnswerGrid = document.getElementById("quiz-answer-grid");
-        quizAnswerGrid.style.gridTemplateColumns = "1fr 0fr"
-        quizAnswerGrid.style.columnGap='0rem';
+        quizBirdDescription.classList.replace("quiz-description", "quiz-description-hidden");
 
-        event.currentTarget.setAttribute("hidden", true);
+        setTimeout(function () {
+                quizBirdDescription.classList.add("hidden");
+            }, 400);
+        event.currentTarget.style.visibility = "hidden";
+
+        event.currentTarget.style.opacity = 0;
+
         quizQuery.refetch();
         setAllAnswersCounter(allAnswersCounter + 1);
         setQuizAnswered(false);
         for (const single_bird of quizGame.birds) {
             let answer_item = document.getElementById("col_quiz_ans_" + single_bird.latin_name);
-            answer_item.classList.add("answer-button-gray")
-            answer_item.classList.remove("answer-button-red")
-            answer_item.classList.remove("answer-button-green")
+            answer_item.classList.add("button-gray")
+            answer_item.classList.remove("button-red")
+            answer_item.classList.remove("button-green")
         }
-
         setTimeout(function () {
             quizBirdSound.play();
         }, 1000);
@@ -147,16 +153,18 @@ function QuizAnswers({quizQuery}: { quizQuery: UseQueryResult }) {
 
     return (
         <>
-            <div className="flex flex-row flex-initial justify-center w-full">
-                <div id="quiz-answer-grid" className="answer-grid  ">
+            <div className="flex flex-row flex-quiz-box justify-center w-full gap-8">
+                <div className="flex flex-col justify-evenly duration-500">
                     {quizAnswers}
-                    <div id="quiz-bird-description"
-                         className="w-full col-start-2 col-end-2 row-start-1 row-end-5 outline p-5 outline-gray-500 outline-2 bg-gray-100"
-                         hidden={true}>{birdDescription}</div>
                 </div>
+                <div id="quiz-bird-description"
+                     className="quiz-description-hidden hidden outline p-5 outline-gray-500 outline-2 bg-gray-100"
+                >{birdDescription}</div>
+
             </div>
-            <button id="new_quiz_button" className="start-new-game-button m-1" hidden={true}
+            <button id="new_quiz_button" className="start-new-game-button m-1"
                     onClick={(e: MouseEvent) => {
+
                         startNewQuiz(e)
                     }}>Start new game
             </button>
@@ -185,7 +193,7 @@ export default function QuizWindow() {
 
     return (
         <>
-            <div className="flex flex-col flex-initial justify-items-start items-center gap-3">
+            <div className="flex flex-col justify-items-start items-center gap-3">
                 <QuizAnswers quizQuery={postQuery}/>
             </div>
         </>
